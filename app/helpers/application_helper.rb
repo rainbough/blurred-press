cec9dbac8771efdc52cfb589e869b5a78a6382fe
Rainbough Phillips <rainbough@gmail.com>
2013-10-23 10:01:31 -0500

    add helper methods to show new post view when user is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def current_user
     session[:user]
   end
+
+# Displays wrapped content if a user is logged in.
+#
+# Returns nothing.
+
+  def user_content(&block)
+  	capture(&block) if current_user
+  end
 end
+
